a488115c7b28960f1571e7ba7a2aa1ef555c1866
Greg Rubin <grubin@catchpoint.com>
2017-06-06 14:24:50 -0400

    validates our form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -24,8 +24,8 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -38,6 +38,24 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  //Validate the inputs from values
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter a category!";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content!";
+  }
+  // If errors is empty, form is fine to submit
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
