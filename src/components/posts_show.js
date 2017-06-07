9658d360a840eb0034119218b23aa8e30258e5ac
Greg Rubin <grubin@catchpoint.com>
2017-06-07 15:01:36 -0400

    Posts show via URL!

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -4,21 +4,29 @@ import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
  componentDidMount() {
-   const { id } = this.props.match.params.id;
+   const { id } = this.props.match.params;
    this.props.fetchPost(id);
  }
 
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Posts Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
 }
 
 function mapStateToProps({ posts }, ownProps) {
-  return { posts: posts[ownProps.match.params.id] };
+  return { post: posts[ownProps.match.params.id] };
 }
 
 export default connect(mapStateToProps, { fetchPost })(PostsShow);
